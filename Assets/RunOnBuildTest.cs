43a8ec0c347db9f6dd5617a0f1c50472b4f20a83
LiveDimensions <livedimensions@gmail.com>
2022-12-09 01:31:31 -0600

    Initial commit

---
+++ Assets/RunOnBuildTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRFastScripting;
+
+public class RunOnBuildTest
+{
+    [RunOnBuild]
+    private static void PrivateTest()
+    {
+        Debug.Log("PrivateTest");
+    }
+
+    [RunOnBuild]
+    private static void PublicTest()
+    {
+        Debug.Log("PublicTest");
+    }
+}
+
+public static class StaticClassRunOnBuildTest
+{
+    [RunOnBuild]
+    private static void PrivateTest()
+    {
+        Debug.Log("PrivateTest");
+    }
+
+    [RunOnBuild]
+    private static void PublicTest()
+    {
+        Debug.Log("PublicTest");
+    }
+}
---
+++ Assets/TestUdonSharp.cs
@@ -0,0 +1,14 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRFastScripting;
+
+[Singleton] [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class TestUdonSharp : UdonSharpBehaviour
+{
+    [SerializeField] [GetComponent] private BoxCollider[] players;
+    [SerializeField] [GetComponentInChildren] private BoxCollider[] players2;
+
+    [SerializeField] private TestUdonSharp test;
+}
