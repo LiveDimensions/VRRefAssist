485678925a73d964b8a6f3b7626f5123c414e6f4
LiveDimensions <livedimensions@gmail.com>
2023-08-11 20:53:41 -0600

    Fixed and cleanup field main automation logic

--- Packages/com.livedimensions.vrrefassist/Editor/Automation/FieldAutomation.cs
+++ Packages/com.livedimensions.vrrefassist/Editor/Automation/FieldAutomation.cs
@@ -19,9 +19,9 @@ namespace VRRefAssist.Editor.Automation
             List<Type> autosetAttributes = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(a => a.GetTypes())
                 .Where(t => !t.IsAbstract && t.IsSubclassOf(typeof(AutosetAttribute))).ToList();
-            
+
             FieldAutomationTypeResults.Clear();
-            
+
             foreach (var autosetAttribute in autosetAttributes)
             {
                 FieldAutomationTypeResults.Add(autosetAttribute, 0);
@@ -52,14 +52,14 @@ namespace VRRefAssist.Editor.Automation
                 }
 
                 count++;
-                
+
                 foreach (var fieldAutomation in FieldAutomationTypeResults.Keys.ToList())
                 {
                     int resultCount = sceneUdon.SetComponentFieldsWithAttribute(fieldAutomation);
                     FieldAutomationTypeResults[fieldAutomation] += resultCount;
                 }
             }
-            
+
             EditorUtility.ClearProgressBar();
 
             foreach (var result in FieldAutomationTypeResults)
@@ -71,26 +71,27 @@ namespace VRRefAssist.Editor.Automation
 
         private static int SetComponentFieldsWithAttribute(this UdonSharpBehaviour sceneUdon, Type automationType)
         {
-            int count = 0;
-            foreach (var field in GetAllFields(sceneUdon.GetType()))
-            {
-                var attributeType = FieldAutomationTypeResults[automationType];
+            FieldInfo[] fields = GetAllFields(sceneUdon.GetType()).ToArray();
 
+            int count = 0;
+            foreach (FieldInfo field in fields)
+            {
                 AutosetAttribute customAttribute = (AutosetAttribute)field.GetCustomAttribute(automationType);
                 if (customAttribute == null) continue;
                 if (!field.IsSerialized()) continue;
-
 
                 if (customAttribute.dontOverride && field.GetValue(sceneUdon) != null)
                 {
                     continue;
                 }
 
-                object[] components = sceneUdon.GetFromFieldAutomation(field.FieldType.IsArray ? field.FieldType.GetElementType() : field.FieldType, customAttribute);
+                bool isArray = field.FieldType.IsArray;
+
+                object[] components = customAttribute.GetObjectsLogic(sceneUdon, isArray ? field.FieldType.GetElementType() : field.FieldType);
 
                 bool failToSet;
 
-                if (field.FieldType.IsArray)
+                if (isArray)
                 {
                     failToSet = components.Length == 0;
                 }
@@ -101,29 +102,29 @@ namespace VRRefAssist.Editor.Automation
 
                 if (failToSet)
                 {
-                    VRRADebugger.LogError($"Failed to set [{attributeType}] ({field.DeclaringType}) {field.Name} on ({sceneUdon.GetType()}) {sceneUdon.name}", sceneUdon);
+                    VRRADebugger.LogError($"Failed to set [{automationType}] ({field.DeclaringType}) {field.Name} on ({sceneUdon.GetType()}) {sceneUdon.name}", sceneUdon);
 
                     if (showPopupWhenFieldAutomationFailed)
                     {
                         bool result = EditorUtility.DisplayDialog("Field Automation...",
-                            $"Failed to set [{attributeType}] ({field.DeclaringType}) {field.Name} on ({sceneUdon.GetType()}) {sceneUdon.name}",
+                            $"Failed to set [{automationType}] ({field.DeclaringType}) {field.Name} on ({sceneUdon.GetType()}) {sceneUdon.name}",
                             "Continue", "Abort");
 
                         if (result) continue;
-                        
+
                         throw new Exception("Field Automation Aborted");
                     }
-                    
+
                     continue;
                 }
 
                 object obj;
-                if (field.FieldType.IsArray)
+                if (isArray)
                 {
                     var elementType = field.FieldType.GetElementType();
 
                     var actualValues = Array.CreateInstance(elementType, components.Length);
-                    
+
                     Array.Copy(components, actualValues, components.Length);
 
                     obj = actualValues;
@@ -141,73 +142,13 @@ namespace VRRefAssist.Editor.Automation
 
             return count;
         }
-        
+
         public static IEnumerable<FieldInfo> GetAllFields(Type t)
         {
             if (t == null) return Enumerable.Empty<FieldInfo>();
-            
+
             return t.GetFields(FieldFlags).Concat(GetAllFields(t.BaseType));
         }
-
-
-        //TODO: Add support for GameObject arrays with [Find] attributes
-        [SuppressMessage("ReSharper", "CoVariantArrayConversion")]
-        private static object[] GetFromFieldAutomation(this UdonSharpBehaviour component, Type type, AutosetAttribute attribute)
-        {
-            return attribute.GetObjectsLogic(component, type);
-            
-            /*
-            switch (automationType)
-            {
-                case FieldAutomationType.GetComponent:
-                    return component.GetComponents(type);
-                case FieldAutomationType.GetComponentInChildren:
-                    return component.GetComponentsInChildren(type, true);
-                case FieldAutomationType.GetComponentInParent:
-                    return component.GetComponentsInParent(type, true);
-                case FieldAutomationType.GetComponentInDirectParent:
-                    return component.transform.parent == null ? Array.Empty<Component>() : component.transform.parent.GetComponents(type);
-                case FieldAutomationType.FindObjectOfType:
-                    return ((FindObjectOfType) attribute).includeDisabled ? UnityEditorExtensions.FindObjectsOfTypeIncludeDisabled(type) : UnityEngine.Object.FindObjectsOfType(type);
-                case FieldAutomationType.Find:
-                    GameObject findGo = GameObject.Find(((Find) attribute).searchName);
-
-                    if (type == typeof(GameObject)) return new object[] {findGo};
-
-                    return findGo == null ? Array.Empty<Component>() : findGo.GetComponents(type);
-                case FieldAutomationType.FindInChildren:
-                    GameObject findInChildrenGo = component.transform.Find(((FindInChildren) attribute).searchName).gameObject;
-
-                    if (type == typeof(GameObject)) return new object[] {findInChildrenGo};
-
-                    return findInChildrenGo == null ? Array.Empty<Component>() : findInChildrenGo.GetComponents(type);
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(automationType), automationType, null);
-            }*/
-        }
-
-        /*
-        private static readonly Dictionary<FieldAutomationType, int> FieldAutomationResults = new Dictionary<FieldAutomationType, int>
-        {
-            {FieldAutomationType.GetComponent, 0},
-            {FieldAutomationType.GetComponentInChildren, 0},
-            {FieldAutomationType.GetComponentInParent, 0},
-            {FieldAutomationType.GetComponentInDirectParent, 0},
-            {FieldAutomationType.FindObjectOfType, 0},
-            {FieldAutomationType.Find, 0},
-            {FieldAutomationType.FindInChildren, 0},
-        };
-
-        private enum FieldAutomationType
-        {
-            GetComponent,
-            GetComponentInChildren,
-            GetComponentInParent,
-            GetComponentInDirectParent,
-            FindObjectOfType,
-            Find,
-            FindInChildren
-        }*/
 
         public static bool IsSerialized(this FieldInfo field) => !(field.GetCustomAttribute<NonSerializedAttribute>() != null || field.IsPrivate && field.GetCustomAttribute<SerializeField>() == null);
     }
