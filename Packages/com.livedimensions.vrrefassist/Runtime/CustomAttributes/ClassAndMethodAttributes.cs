ecc124b0ddfbd277cc0f9120898ae626b7206199
LiveDimensions <livedimensions@gmail.com>
2023-12-28 16:13:34 -0600

    Updated summary for RunOnBuild

--- Packages/com.livedimensions.vrrefassist/Runtime/CustomAttributes/ClassAndMethodAttributes.cs
+++ Packages/com.livedimensions.vrrefassist/Runtime/CustomAttributes/ClassAndMethodAttributes.cs
@@ -12,14 +12,15 @@ namespace VRRefAssist
     }
 
     /// <summary>
-    /// Any static method with this attribute will be called on build.
+    /// Any method with this attribute will be called on build.
+    /// Static and instance methods are supported.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)] [MeansImplicitUse]
     public class RunOnBuildAttribute : Attribute
     {
         public readonly int executionOrder;
         
-        /// <param name="executionOrder">Execution order for RunOnBuild methods, lower values execute first.</param>
+        /// <param name="executionOrder">Execution order for RunOnBuild methods, lower values execute first. Values higher than 1000 will execute after field-automation</param>
         public RunOnBuildAttribute(int executionOrder = 0)
         {
             this.executionOrder = executionOrder;
