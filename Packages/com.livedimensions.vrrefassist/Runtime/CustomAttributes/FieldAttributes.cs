e91ba26815b5b01caa0b77f4c87f047d3cd8409b
KitKat <katjastoroygard@gmail.com>
2026-09-10 03:11:54 +0200

    Update FieldAttributes.cs

--- Packages/com.livedimensions.vrrefassist/Runtime/CustomAttributes/FieldAttributes.cs
+++ Packages/com.livedimensions.vrrefassist/Runtime/CustomAttributes/FieldAttributes.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using VRRefAssist.AttributeUtils;
+
 // ReSharper disable CoVariantArrayConversion
 
 namespace VRRefAssist
@@ -315,9 +317,7 @@ namespace VRRefAssist
         
         public override object[] GetObjectsLogic(MonoBehaviour monoBehaviour, Type type, FieldInfo field)
         {
-            if (!field.FieldType.IsArray) throw new ArgumentException($"Field '{field.Name}' must be an array!");
-            
-            var values = (object[])field.GetValue(monoBehaviour);
+            object[] values = field.GetValues(monoBehaviour);
             if (values == null) return Array.Empty<object>();
             return values.Distinct().Where(x => !x.Equals(null)).ToArray();
         }
